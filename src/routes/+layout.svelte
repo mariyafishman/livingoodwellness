49c5444f068c2d547678c6d1d1943b8d4280fe6a
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-08-25 21:26:02 -0400

    Add sonner component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '#lib/assets/favicon.svg';
+  import './layout.css';
+  import favicon from '#lib/assets/favicon.svg';
+  import { ModeWatcher } from 'mode-watcher';
+  import { Toaster } from '#lib/components/ui/sonner/index.js';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
+
+<ModeWatcher defaultMode="light" />
+
+<Toaster richColors />
 
 {@render children()}
