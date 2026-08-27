1340b213b6b229aeb7d9a0ff18e4c59c31a43ec4
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-08-26 22:52:25 -0400

    Add SEO component

---
+++ src/lib/components/seo.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { site } from '#lib/config/site.ts';
+
+  interface Props {
+    title?: string;
+    description?: string;
+  }
+
+  let {
+    title,
+    description = `${site.description}`
+  }: Props = $props();
+
+  const fullTitle = $derived(
+    title ? `${title} - ${site.name}` : `${site.name} - ${site.tagline}`
+  )
+</script>
+
+<svelte:head>
+  <title>{fullTitle}</title>
+  <meta name="description" content="{description}" />
+  <meta property="og:title" content="{fullTitle}" />
+  <meta property="og:description" content="{description}" />
+</svelte:head>
